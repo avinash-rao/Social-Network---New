5dea9892c8d4a1ad076363df62078cbf9d080a05
jeetpatel2059 <jeetlalkia@gmail.com>
2021-05-27 16:15:23 +0530

    name update bug fix

--- main/SocialNetwork.cpp
+++ main/SocialNetwork.cpp
@@ -321,15 +321,15 @@ void Pages::userHomePage() {
 
 // Page 5
 void Pages::editProfilePage() {
-    string options[] = {"Name: "+allUsers[currentUser].getName(), 
+    string temp;
+    int choice, tempAge;
+    do {
+        string options[] = {"Name: "+allUsers[currentUser].getName(), 
                         "Age: " + to_string(allUsers[currentUser].getAge()), 
                         "Location: " + allUsers[currentUser].getLocation(), 
                         "Password", 
                         "Go back"};
-    int optionSize = 5;
-    string temp;
-    int choice, tempAge;
-    do {
+         int optionSize = 5;
         showMenuOptions(options, optionSize);
         cout << "Enter your choice: ";
         cin >> choice;
